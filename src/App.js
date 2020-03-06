e2477d748e0ec5b1df92d27db957bf55405ed808
Michael Padiernos <mike.padiernos@gmail.com>
2020-03-06 16:44:52 -0500

    FEATURE:(Asset & Actions): CoreUI Icons & Actions Button
    - imported CoreUI Icons into assets
    - coded Actions Button (FAB) when logged in (non - functional except logout)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, useHistory } from "react-router-dom";
 
 // IMPORT ASSETS
 // import './css/App.css'
-// import './assets/coreui/css/all.min.css'
+import './assets/coreui/css/all.min.css'
 
 // IMPORT UTILITIES
 import ProtectedRoute                                   from "./utilities/ProtectedRoute"
