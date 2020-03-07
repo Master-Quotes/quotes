0b1144df2203043f4a52d2153d5ee7ec0ce1b147
Alex Magana <46731475+jamcoding@users.noreply.github.com>
2020-03-06 22:52:41 -0600

    fixed errors

--- src/components/Quote/QuoteCard.js
+++ src/components/Quote/QuoteCard.js
@@ -4,16 +4,13 @@ const QuoteCard = () => {
 	return (
 		<article className="card-quote">
 			<div className="card-body">
-<<<<<<< HEAD
 				"(Quote & Speaker)"
 				<h3>{props.speaker}</h3>
 				<p>{props.quote}</p>
-=======
 				"(Quote)"
 			</div>
 			<div className="card-footer">
 				"(Speaker)"
->>>>>>> a58d732147b95c92c7c333a658ac886e2f355a46
 			</div>
 		</article>
 	)
