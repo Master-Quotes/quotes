8deb2d99cb1b782b0510c871e9ea5355d408c368
Michael Padiernos <mike.padiernos@gmail.com>
2020-03-06 23:03:45 -0500

    STYLE:(Login): Form styling
    - add style to login form

--- src/components/Content/ContentSessionOut.js
+++ src/components/Content/ContentSessionOut.js
@@ -3,11 +3,19 @@ import { Link } from "react-router-dom";
 
 const ContentSessionOut = () => {
 	return (
-		<section className="section section-container is-flex-centered is-full-height">
-			<p>
-				Are you a member?<br />
-				If not, register below!
-			</p>
+		<section className="section section-login login-container is-flex-centered is-full-height">
+			<div className="login-body">
+				<ul>
+					<li>Are you a member?</li>
+					<li>If not, register below!</li>
+				</ul>
+				{/*<p>*/}
+				{/*	Are you a member?*/}
+				{/*</p>*/}
+				{/*<p>*/}
+				{/*	If not, register below!*/}
+				{/*</p>*/}
+			</div>
 			<span className="button-group">
 				<Link to={`/user/login`} className="button">Login</Link>
 				<Link to={`/user/register`} className="button">Register</Link>
