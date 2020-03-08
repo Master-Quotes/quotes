c669a7e7d8bbb2e41c7c3df0e5bb9c1b41bf836b
Michael Padiernos <mike.padiernos@gmail.com>
2020-03-08 18:01:54 -0400

    FEATURE:(Form): Add Quote Form
    - updated form shape adding heading and row / col for textarea

--- src/components/Quote/QuoteList.js
+++ src/components/Quote/QuoteList.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useContext } from "react";
+import Pusher from "pusher-js"
 
 // IMPORT UTILITIES
 import { axiosWithAuth } from "../../utilities/axiosWithAuth";
@@ -14,14 +15,30 @@ const QuoteList = () => {
 	const { quotes, setQuotes } = useContext(QuoteContext);
 	console.log("Quotes: ", quotes);
 
+	const getQuotes = _ => {
+		axiosWithAuth()
+			.get('/quotes/')
+			.then(response => {
+				console.log("Response: ", response.data);
+				setQuotes(response.data);
+			})
+			.catch(error => console.log('Error: ', error));
+	};
+
 	useEffect(() => {
-			axiosWithAuth()
-				.get('/quotes/')
-				.then(response => {
-					console.log("Response: ", response.data);
-					setQuotes(response.data);
-				})
-				.catch(error => console.log('Error: ', error));
+
+		getQuotes();
+
+		const pusher = new Pusher('326bd157958340453bb2', {
+			cluster: 'us2',
+			forceTLS: true
+		});
+
+		const channel = pusher.subscribe('quotes');
+		channel.bind('new-quote-data', function(data) {
+			getQuotes();
+		});
+
 		}, []);
 
 	const quotesMap = quotes.reverse();
