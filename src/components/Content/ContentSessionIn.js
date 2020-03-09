0e99fe3d977896a6b22dcec415f7cd9fd84288e9
Michael Padiernos <mike.padiernos@gmail.com>
2020-03-09 09:11:33 -0400

    FEATURE:(Home): Memo
    - memoize user info

--- src/components/Content/ContentSessionIn.js
+++ src/components/Content/ContentSessionIn.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useContext, useMemo } from "react";
+
+// IMPORT CONTEXTS
+import UserContext from "../../context/UserContext";
 
 // IMPORT APP COMPONENTS
 import QuotesFeatured from "../Quote/QuoteFeatured";
 
 const ContentSessionIn = () => {
+	const {login, setLogin } = useContext(UserContext);
+	console.log(login);
+
+	// MEMOIZE STATE
+	const userValue = useMemo(() => ({ login, setLogin }), [login, setLogin]);
+
 	return (
-		<section className="section section-container container">
+		<section className="section section-home home-container container">
+			<div className="home-title">
+				<h2>Hi, {login.username}!</h2>
+			</div>
 			<QuotesFeatured />
 		</section>
 	)
